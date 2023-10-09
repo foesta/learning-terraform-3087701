f696fcdd7640a56307bb3e6b5e10782dbbf2d5c6
foesta <burn_foesa@protonmail.com>
2023-10-09 17:22:28 +0100

    Another small one

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.web_vpc.public_subnets[0]
   vpc_security_group_ids = [module.web_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
